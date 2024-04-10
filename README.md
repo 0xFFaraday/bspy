b36ad8795c415c15a84c1a3ac52909e88074a30a
0xFFaraday <89433963+0xFFaraday@users.noreply.github.com>
2024-04-10 00:25:55 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# bspy - Browser Spy
+Name inspired from the wonderful tool [pspy](https://github.com/DominicBreuker/pspy)!
+
+## Overview
+Provide security researchers or security analysts the abilitiy to parse browser history in a more accessible manner without the use of SQLite GUI.
+
+## Features
+- Automatic extraction of potential IoCs
+- Chainsaw-inspired output for console
+
+### References
+
+https://jacobnarayan.com/blogs/how-to-find-the-most-recent-file-in-a-directory-in-python
+https://docs.nxlog.co/integrate/browser-history.html
---
+++ main.py
@@ -0,0 +1,53 @@
+import sqlite3
+import os
+from pathlib import Path
+
+
+def getNewestFile():
+    directory_path = str(Path.home()) + "\\Downloads"
+    most_recent_file = None
+    most_recent_time = 0
+
+    # iterate over the files in the directory
+    for entry in os.scandir(directory_path):
+        if entry.name.endswith(".sqlite"):
+            mod_time = entry.stat().st_mtime_ns
+            if mod_time > most_recent_time:
+                # update the most recent file and its modification time
+                most_recent_file = entry.name
+                most_recent_time = mod_time
+
+    return os.path.join(directory_path, most_recent_file)
+
+
+def dbConnection(database):
+    con = sqlite3.connect(database)
+    cur = con.cursor()
+    return cur, con
+
+def dbCleanup(con):
+    con.close()
+
+
+def queryDownloads(connection):
+    # The result of a "cursor.execute" can be iterated over by row
+    downloads = connection.execute("SELECT id, current_path, DATETIME(ROUND(start_time / 1000000-11644473600), 'unixepoch', 'localtime') AS EventTime, referrer, site_url, tab_url, tab_referrer_url, mime_type, original_mime_type, total_bytes FROM downloads;")
+    #columnNames = list(map(lambda x: x[0], cursor.description))
+    #print(columnNames)
+
+    for download in downloads:
+        print(download)
+
+
+def main():
+    databaseHistory = getNewestFile()
+    
+    cursor, con = dbConnection(databaseHistory)
+    
+    queryDownloads(cursor)
+    dbCleanup(con)
+    print(f"Read file: {databaseHistory}")
+    print("Database connection closed...")
+
+if __name__ == "__main__":
+    main()
