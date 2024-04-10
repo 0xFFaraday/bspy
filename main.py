a133140a85eafc64121daed86abf20810e24babd
Faraday <89433963+0xFFaraday@users.noreply.github.com>
2024-04-10 14:17:04 -0400

    Feature: Table Output
    Text length for URLs needs to be fixed.

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ import os
 from pathlib import Path
 import typer
 from rich.console import Console
-from rich.table import Table
+from rich.table import Table, Column
 
 console = Console()
 
 def getNewestFile():
-    directory_path = str(Path.home()) + "\\Downloads"
+    directory_path = str(Path.home()) + "/Downloads"
     most_recent_file = None
     most_recent_time = 0
 
@@ -35,23 +35,46 @@ def dbCleanup(con):
 
 def queryDownloads(connection):
     # The result of a "cursor.execute" can be iterated over by row
-    downloads = connection.execute("SELECT id, current_path, DATETIME(ROUND(start_time / 1000000-11644473600), 'unixepoch', 'localtime') AS EventTime, referrer, site_url, tab_url, tab_referrer_url, mime_type, original_mime_type, total_bytes FROM downloads;")
-    #columnNames = list(map(lambda x: x[0], cursor.description))
+    downloads = connection.execute("SELECT id, current_path, DATETIME(ROUND(start_time / 1000000-11644473600), 'unixepoch', 'localtime') AS EventTime, referrer, site_url, tab_url, tab_referrer_url, mime_type, original_mime_type FROM downloads;")
+    #columnNames = list(map(lambda x: x[0], connection.description))
     #print(columnNames)
 
-    for download in downloads:
-        print(download)
+    return downloads
 
+def printOutput(downloads: tuple):
+
+    downloadsTable = Table("ID", "File Path", "Date", "Referrer", "Site URL", "Tab URL", "Tab Referrer URL", "mime_type", "original_mime_type", Column(no_wrap=False), show_lines=True)
+
+    for download in downloads:
+        dataNormalized = {
+            "ID": str(download[0]),
+            "File_Path": str(download[1]),
+            "Date": str(download[2]),
+            "Referrer": str(download[3]),
+            "Site_URL": str(download[4]),
+            "Tab_URL": str(download[5]),
+            "Tab_Referrer_URL": str(download[6]),
+            "Mime_Type": str(download[7]),
+            "Original_Mime_Type": str(download[8])
+    }
+        downloadsTable.add_row(dataNormalized["ID"], dataNormalized["File_Path"], dataNormalized["Date"], dataNormalized["Referrer"], dataNormalized["Site_URL"], dataNormalized["Tab_URL"], dataNormalized["Tab_Referrer_URL"], dataNormalized["Mime_Type"], dataNormalized["Original_Mime_Type"])
+
+    if downloadsTable.columns:
+        console.print(downloadsTable)
+    else:
+        console.print("[i]No data...[/i]")
 
 def main():
     databaseHistory = getNewestFile()
     
     cursor, con = dbConnection(databaseHistory)
+    console.print(f"Reading file - {databaseHistory}", style="white on blue")
     
-    queryDownloads(cursor)
+    downloads = queryDownloads(cursor)
+    printOutput(downloads)
+    
     dbCleanup(con)
-    print(f"Read file: {databaseHistory}")
-    print("Database connection closed...")
+    console.print("Database connection closed...", style="white on blue")
 
 if __name__ == "__main__":
-    typer.run(main())
+    typer.run(main)
